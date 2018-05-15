beaad5f8a5ff1a3e73d089500b8d9c7a5734ec78
dsoft-tw <timw@dsoft-tech.com>
2018-05-15 11:14:31 -0600

    Initial commit of base .net core 2 app with EF

---
+++ src/DSoft.AgileSprinter.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSoft.AgileSprinter.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
